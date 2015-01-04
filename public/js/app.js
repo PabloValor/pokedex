4fc986fc3fe2880ef190b56f8896229496b51b99
Pablo Valor <pablovalor89@gmail.com>
2015-01-04 19:09:00 -0300

    Added Stats panel

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,17 @@
 			type: ['Grass', 'Poison'],
 			height: "2'4",
 			weight: '15.2 lbs',
-			abillities: ['Overgrow', 'Chlorophyll']
-		}
-	})
+			abillities: ['Overgrow', 'Chlorophyll'],
+			stats: {
+				hp: 45,
+				attack: 49,
+				defense: 49,
+				"sp.atk": 65,
+				"sp.def": 65,
+				speed: 45,
+				total: 318
+			},
+			evolution: ["Bulbasour", "Ivysaur", "Venusaur"]
+		};
+	});
 })();
