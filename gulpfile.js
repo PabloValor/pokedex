e982d858b6126745e593d49ec78e77c33a0184ef
Pablo Valor <pablovalor89@gmail.com>
2015-01-04 13:25:18 -0300

    Added boostrap, boostwacht theme and commented button styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
 var gulp = require('gulp'),
 	less = require('gulp-less'),
-	refresh = require('gulp-livereload');
+	refresh = require('gulp-livereload'),
+	gutil = require('gulp-util');
 
 
 gulp.task('less', function() {
-	gulp.src('./public/css/*.less')
-		.pipe(less())
+	gulp.src('./public/css/main.less')
+		.pipe(less({compress: true}))
+		.on('error', gutil.log)
 		.pipe(gulp.dest('./public/build/css'))
 		.pipe(refresh());
 });
 
 gulp.task('watch', function() {
 	refresh.listen();
-	gulp.watch('public/css/*.less', ['less']);
+
+	gulp.watch('public/**/*.less', ['less']);
 });
 
 gulp.task('default', ['less', 'watch']);
