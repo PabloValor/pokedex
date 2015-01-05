ccfef69d984a365ebaae35118bd818b9b0d4479f
Pablo Valor <pablovalor89@gmail.com>
2015-01-05 19:29:01 -0300

    config  new routes

--- public/js/app.js
+++ public/js/app.js
@@ -9,9 +9,15 @@
 	app.config(['$routeProvider', function($routeProvider) {
 		$routeProvider
 			.when('/', {
+				templateUrl: 'views/pokedex.html'
+			})
+			.when('/pokemon/:id', {
 				templateUrl: 'views/pokemon.html',
 				controller: 'PokemonController',
 				controllerAs: 'pkmCtrl'
+			})
+			.otherwise({
+				redirectTo: '/'
 			});
 	}]);
 
