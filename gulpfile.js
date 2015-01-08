d2a9b7a89863577a56503f983a81008bdbe227fe
Pablo Valor <pablovalor89@gmail.com>
2015-01-08 19:03:07 -0300

    Added new features on gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,29 @@ var gulp = require('gulp'),
 	less = require('gulp-less'),
 	refresh = require('gulp-livereload'),
 	uglify = require('gulp-uglify'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	concat = require('gulp-concat'),
+	rename = require('gulp-rename');
 
 gulp.task('compress-javascript', function() {
-	gulp.src('./public/js/**/*.js')
+	gulp.src([
+			'./public/js/lib/underscore.min.js',
+			'./public/js/lib/angular-route.min.js',
+			'./public/js/*.js'
+		])
+		.pipe(concat('pokedex.js'))
 		.pipe(uglify())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('./public/build/js/'))
 		.on('error',gutil.log)
-		.pipe(gulp.dest('./public/build/js'))
 		.pipe(refresh());
 });
 
-
 gulp.task('less', function() {
-	gulp.src('./public/css/main.less')
+	gulp.src('./public/css/pokedex.less')
 		.pipe(less({compress: true}))
 		.on('error', gutil.log)
+		.pipe(rename({suffix: '.min'}))
 		.pipe(gulp.dest('./public/build/css'))
 		.pipe(refresh());
 });
