2c8b8823d5c1253feb430954373b0b3e1a39a149
Pablo Valor <pablovalor89@gmail.com>
2015-01-08 18:15:13 -0300

    Added partition hack method to use with boostrap classes

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,4 +1,4 @@
-(function() {
+(function(_) {
 	angular.module('pokedex.controllers', [])
 
 	.controller('PokedexController', ['$scope', '$routeParams', 'pokemonService', function($scope, $routeParams, pokemonService) {
@@ -8,13 +8,23 @@
 		if(type) {
 			pokemonService.byType(type).then(function(data) {
 				$scope.pokemons = data;
+				$scope.type = type;
+				$scope.groupped = partition(data, 4);
 			});
 
 		} else {
 			pokemonService.all().then(function(data) {
 				$scope.pokemons = data;
+				$scope.groupped = partition(data, 4);
 			});
 		}
+
+		function partition(data, n) {
+			return _.chain(data).groupBy(function(element, index){
+				return Math.floor(index / n);
+			}).toArray().value();
+		}
+
 	}])
 
 	.controller('PokemonController', ['$scope', '$routeParams', 'pokemonService', function($scope, $routeParams, pokemonService) {
@@ -35,4 +45,4 @@
 		}
 	});
 
-})();
+})(_);
