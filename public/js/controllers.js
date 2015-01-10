4be1d63bd02dfcab12efe0093afa3b4b43e5289c
Pablo Valor <pablovalor89@gmail.com>
2015-01-10 19:18:29 -0300

    Implemented search function

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -8,17 +8,30 @@
 
 		if(type) {
 			pokemonService.byType(type).then(function(data) {
-				$scope.pokemons = data;
+				$scope.pokemons = pokemons = data;
 				$scope.type = type;
 				$scope.groupped = partition(data, 4);
 			});
 
 		} else {
 			pokemonService.all().then(function(data) {
-				$scope.pokemons = data;
+				$scope.pokemons = pokemons = data;
 				$scope.groupped = partition(data, 4);
 			});
 		}
+
+		$scope.search = function() { 
+			var result = pokemons;
+			if($scope.searchTerm) {
+				result = pokemons.filter(function(pokemon) {
+					var name = pokemon && pokemon.name || "";
+					return name.toLowerCase().indexOf($scope.searchTerm.toLowerCase()) !== -1;
+				});
+			}
+
+			$scope.pokemons = result;
+			$scope.groupped = partition(result, 4);
+		};
 
 		function partition(data, n) {
 			return _.chain(data).groupBy(function(element, index){
